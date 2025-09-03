4bc6badfc642c0cd286095f464d9328d3960cd5c
khalid <ga.lid2002.km@gmail.com>
2025-09-03 22:50:10 +0100

    feat: implement decodeList with relevant test

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"strconv"
 )
 
-func decodeInt(s string) (int, error) {
+func decodeInt(s string) (int, int, error) {
+	// i4e
 	var lastIndex int
 	for i := 1; i < len(s); i++ {
 		if s[i] == 'e' {
@@ -14,17 +15,17 @@ func decodeInt(s string) (int, error) {
 		}
 	}
 	if lastIndex == 0 {
-		return 0, fmt.Errorf("no end character found in s: %v", s)
+		return 0, 0, fmt.Errorf("no end character found in s: %v", s)
 	}
 	result, err := strconv.Atoi(s[1:lastIndex])
 	if err != nil {
-		return 0, fmt.Errorf("Failed to parse %v as integer", s)
+		return 0, 0, fmt.Errorf("failed to parse %v as integer", s)
 	}
-	return result, nil
+	return result, lastIndex + 1, nil
 }
 
-func decodeByteString(s string) ([]byte, error) {
-	//8:elephant
+func decodeByteString(s string) ([]byte, int, error) {
+	// 8:elephant
 	var colonIndex int
 	for i := 0; i < len(s); i++ {
 		if s[i] == ':' {
@@ -34,40 +35,72 @@ func decodeByteString(s string) ([]byte, error) {
 	}
 
 	if colonIndex == 0 {
-		return nil, fmt.Errorf("no length found before colon in s: %v", s)
+		return nil, 0, fmt.Errorf("no length found before colon in s: %v", s)
 	}
 
 	length, err := strconv.Atoi(s[:colonIndex])
 	if err != nil {
-		return nil, fmt.Errorf("Failed to get length of s: %v", s)
+		return nil, 0, fmt.Errorf("failed to get length of s: %v", s)
 	}
 
 	byteString := make([]byte, length)
 
 	copy(byteString, s[colonIndex+1:colonIndex+1+length])
 
-	return byteString, nil
+	return byteString, colonIndex + 1 + length, nil
 }
 
-func decodeList(s string) (string, error) {
-	return "", nil
+func decodeList(s string) (interface{}, int, error) {
+	// li4e8:elephante
+	var result []interface{}
+	i := 1
+	for i < len(s) && s[i] != 'e' {
+		var consumed int
+		currResult, consumed, err := decode(s[i:])
+		if err != nil {
+			return nil, 0, err
+		}
+		result = append(result, currResult)
+		i += consumed
+	}
+
+	if i >= len(s) || s[i] != 'e' {
+		return nil, 0, fmt.Errorf("list not terminated with 'e'")
+	}
+
+	return result, i + 1, nil
 }
 
 func decodeDict(s string) (string, error) {
+	//
 	return "", nil
 }
 
-func decode(s string) {
+func decode(s string) (interface{}, int, error) {
 	switch firstChar := s[0]; {
+
 	case firstChar == 'i':
-		decodeInt(s)
+		int, consumed, err := decodeInt(s)
+		if err != nil {
+			return nil, 0, fmt.Errorf("Cannot decode: %v", s)
+		}
+		return int, consumed, nil
+
 	case firstChar > '0' && firstChar <= '9':
-		decodeByteString(s)
+		str, consumed, err := decodeByteString(s)
+		if err != nil {
+			return nil, 0, fmt.Errorf("Cannot decode: %v", s)
+		}
+		return str, consumed, nil
+
 	case firstChar == 'l':
-		decodeList(s)
+		return decodeList(s)
+
 	case firstChar == 'd':
 		decodeDict(s)
 	}
+
+	return nil, 0, fmt.Errorf("Cannot decode: %v", s)
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestDecodeInt(t *testing.T) {
 	s := "i52e"
 	expected := 52
-	result, err := decodeInt(s)
+	result, _, err := decodeInt(s)
 	if err != nil {
 		t.Errorf("unexpected error: %v", err)
 	}
@@ -23,13 +24,31 @@ func TestDecodeInt(t *testing.T) {
 func TestDecodeByteString(t *testing.T) {
 	s := "12:hello world!"
 	expected := []byte{'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!'}
-	result, err := decodeByteString(s)
+	result, _, err := decodeByteString(s)
 	if err != nil {
 		t.Errorf("unexpected error: %v", err)
 	}
 
 	if !bytes.Equal(result, expected) {
 		t.Errorf("expected %s but got %s", expected, result)
+	}
+
+	t.Logf("result: %v", result)
+}
+
+func TestDecodeList(t *testing.T) {
+	s := "li12e12:hello world!e"
+	expected := []interface{}{
+		12,
+		[]byte("hello world!"),
+	}
+	result, _, err := decodeList(s)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("expected %v but got %v", expected, result)
 	}
 
 	t.Logf("result: %v", result)
